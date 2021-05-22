3a613d6d6a252ec6bf4efad7f34812fd1af4dcf4
Dimitrios Tselikis <dimitriostselikis@gmail.com>
2021-05-22 18:35:09 +0300

    Added functionality for browse buttons

--- src/main/java/controllers/MainContoller.java
+++ src/main/java/controllers/MainContoller.java
@@ -1,2 +1,55 @@
-package controllers;public class MainContoller {
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainContoller implements Initializable {
+
+    @FXML
+    private TextField destFolderTF;
+    @FXML
+    private Button destFolderBtn;
+    @FXML
+    private TextField ffplayTF;
+    @FXML
+    private Button ffplayBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void destFolderBtn_onClick(ActionEvent e) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Choose the videos directory");
+        File file = directoryChooser.showDialog(destFolderBtn.getScene().getWindow());
+
+        if (file != null) {
+            destFolderTF.setText(file.getAbsolutePath());
+        }
+    }
+
+    public void setFfplayBtn_onClick(ActionEvent e) {
+        FileChooser.ExtensionFilter exeFilter = null;
+        exeFilter = new FileChooser.ExtensionFilter("EXE files (*.exe)", "*.exe");
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select ffplay executable");
+        fileChooser.setInitialFileName("ffplay.exe");
+        fileChooser.getExtensionFilters().add(exeFilter);
+        File file = fileChooser.showOpenDialog(ffplayBtn.getScene().getWindow());
+
+        if (file != null) {
+            ffplayTF.setText(file.getAbsolutePath());
+        }
+    }
 }
