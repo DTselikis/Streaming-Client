d838103195e9ec95fea6f1e7c239cb2f52350c5a
Dimitrios Tselikis <dimitriostselikis@gmail.com>
2021-05-22 16:46:43 +0300

    Impemented main

--- src/main/java/StreamingClient.java
+++ src/main/java/StreamingClient.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class StreamingClient {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StreamingClient extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("StreamingClient.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
 }
