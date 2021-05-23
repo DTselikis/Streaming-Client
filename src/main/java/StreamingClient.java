b4ddcc279556dc6589587f2eb9a7a323da98b1ba
Dimitrios Tselikis <dimitriostselikis@gmail.com>
2021-05-23 20:57:30 +0300

    Added window title

--- src/main/java/StreamingClient.java
+++ src/main/java/StreamingClient.java
@@ -9,6 +9,7 @@ public class StreamingClient extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("StreamingClient.fxml"));
         Scene scene = new Scene(loader.load());
         stage.setScene(scene);
+        stage.setTitle("Streaming client");
         stage.show();
     }
 
