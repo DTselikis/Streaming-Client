c319564e69245b5c8536754d1e3b17b3864b18fc
Dimitrios Tselikis <dimitriostselikis@gmail.com>
2021-05-23 13:26:54 +0300

    Implemented VideoItemController
    Control the behavior of evert grid item

--- src/main/java/controllers/VideoItemController.java
+++ src/main/java/controllers/VideoItemController.java
@@ -1,2 +1,37 @@
-package controllers;public class VideoItemController {
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import listeners.VideoItemClickListener;
+import res.VideoItem;
+
+public class VideoItemController {
+    private VideoItem video;
+    private VideoItemClickListener listener;
+
+    @FXML
+    private Label formatLbl;
+    @FXML
+    private Label resLbl;
+    @FXML
+    private Label titleLbl;
+
+    public void setData(VideoItem video, VideoItemClickListener listener) {
+        this.video = video;
+        this.listener = listener;
+
+        setGUIInfo();
+    }
+
+    @FXML
+    private void click() {
+        listener.onClickListener(this.video);
+    }
+
+    private void setGUIInfo() {
+        formatLbl.setText(this.video.getFormat());
+        titleLbl.setText(this.video.getTitle());
+        resLbl.setText(this.video.getResolution());
+    }
 }
